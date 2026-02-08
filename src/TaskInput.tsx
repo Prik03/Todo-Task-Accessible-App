3aaa68f802415028c9b3a77f55e690cdb83fbf45
Prik03 <princesain9319@gmail.com>
2026-02-09 00:16:24 +0530

    add tailwind and action button
    Instead of using list use table

--- src/TaskInput.tsx
+++ src/TaskInput.tsx
@@ -17,16 +17,23 @@ export default function TaskInput({ onAddTask, buttonText }: TaskInputProps) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="new-task-input">New Task</label>
-      <input
-        value={task}
-        id="new-task-input"
-        type="text"
-        placeholder="Add a new task"
-        onChange={(e) => setTask(e.target.value)}
-      />
-      <button>{buttonText}</button>
+    <form onSubmit={handleSubmit} className="flex items-center flex-wrap">
+      <div className="flex flex-wrap flex-col w-1/4">
+        <label htmlFor="new-task-input">New Task</label>
+        <input
+          value={task}
+          id="new-task-input"
+          type="text"
+          placeholder="Add a new task"
+          onChange={(e) => setTask(e.target.value)}
+          className="border-2 rounded-md p-1"
+        />
+      </div>
+      <div className="ml-3 mt-6">
+        <button className="bg-blue-300 text-gray-800 p-2 font-bold rounded-md">
+          {buttonText}
+        </button>
+      </div>
     </form>
   );
 }
