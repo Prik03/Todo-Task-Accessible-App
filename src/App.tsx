eb2c3418c45887db149636815aa24201d4299caa
Prik03 <princesain9319@gmail.com>
2026-02-10 01:40:22 +0530

    Added notification toastify
    Add edit button

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,50 @@
 import { useState } from 'react';
 import type { Task } from './types';
 import TaskInput from './TaskInput';
+import TaskTable from './TaskTable';
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
-  console.log('app load');
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [editTask, setEditTask] = useState<number | null>(null);
+  const [updatedTask, setUpdatedTask] = useState<string>('');
 
   const onAddTask = (trimTask: string) => {
     setTasks([
       ...tasks,
       { id: Date.now(), title: trimTask, completionStatus: false },
     ]);
+    toast('Task Added');
   };
 
+  const onEditTask = (tasks: Task) => {
+    setEditTask(tasks.id);
+  };
+
+  const onSaveTask = () => {
+    if (!updatedTask.trim()) return;
+
+    setTasks(
+      tasks.map((task) =>
+        task.id === editTask ? { ...task, title: updatedTask } : task,
+      ),
+    );
+    toast('Task Updated');
+    setEditTask(null);
+  };
   return (
     <>
       <div className="App mx-auto w-4/5">
         <h1 className="text-3xl place-self-center">Todo Accessible App</h1>
         <TaskInput onAddTask={onAddTask} buttonText="Add Task" />
-        <div className="overflow-auto">
-          <table className="w-full border-collapse bg-white shadow-lg rounded-md overflow-hidden mt-5">
-            <thead className="bg-linear-to-r from-indigo-900 to-purple-500 text-white">
-              <tr>
-                <th className="text-left px-6 py-3 text-sm font-semibold tracking-wide">
-                  ID
-                </th>
-                <th className="text-left px-6 py-3 text-sm font-semibold tracking-wide">
-                  Task
-                </th>
-                <th className="text-left px-6 py-3 text-sm font-semibold tracking-wide">
-                  Action
-                </th>
-              </tr>
-            </thead>
-
-            <tbody className="text-gray-700 text-sm">
-              {tasks
-                ? tasks.map((task, index) => (
-                    <tr
-                      key={task.id}
-                      className={`border-b last:border-none transition duration-200 hover:bg-indigo-50 ${
-                        index % 2 === 0 ? 'bg-gray-50' : 'bg-white'
-                      }`}
-                    >
-                      <td className="px-6 py-3 font-medium text-indigo-600">
-                        {task.id}
-                      </td>
-                      <td className="px-6 py-3">{task.title}</td>
-                      <td className="px-6 py-3">
-                        <button className="p-2 rounded-md mx-1 bg-blue-300 text-gray-700">
-                          Edit
-                        </button>
-                        <button className="p-2 rounded-md mx-1 bg-green-500 text-gray-800">
-                          Complete
-                        </button>
-                        <button className="p-2 rounded-md mx-1 bg-red-400 text-gray-900">
-                          Delete
-                        </button>
-                      </td>
-                    </tr>
-                  ))
-                : 'No Task Added'}
-            </tbody>
-          </table>
-        </div>
+        <TaskTable
+          tasks={tasks}
+          onEditTask={onEditTask}
+          editTask={editTask}
+          onSaveTask={onSaveTask}
+          setUpdatedTask={setUpdatedTask}
+        />
+        <ToastContainer />
       </div>
     </>
   );
---
+++ src/TaskTable.tsx
@@ -0,0 +1,99 @@
+import type { Task } from './types';
+import { useRef, useEffect } from 'react';
+
+type TableData = {
+  tasks: Task[];
+  onEditTask: (task: Task) => void;
+  editTask: number | null;
+  onSaveTask: () => void;
+  setUpdatedTask: React.Dispatch<React.SetStateAction<string>>;
+};
+
+function TaskTable({
+  tasks,
+  onEditTask,
+  editTask,
+  onSaveTask,
+  setUpdatedTask,
+}: TableData) {
+  const editInputRef = useRef<HTMLInputElement | null>(null);
+  useEffect(() => {
+    if (editTask !== null) {
+      editInputRef.current?.focus();
+    }
+  }, [editTask]);
+  return (
+    <div className="overflow-auto">
+      <table className="w-full border-collapse bg-white shadow-lg rounded-md overflow-hidden mt-5">
+        <thead className="bg-linear-to-r from-indigo-900 to-purple-500 text-white">
+          <tr>
+            <th className="text-left px-6 py-3 text-sm font-semibold tracking-wide">
+              ID
+            </th>
+            <th className="text-left px-6 py-3 text-sm font-semibold tracking-wide">
+              Task
+            </th>
+            <th className="text-left px-6 py-3 text-sm font-semibold tracking-wide">
+              Action
+            </th>
+          </tr>
+        </thead>
+
+        <tbody className="text-gray-700 text-sm">
+          {tasks
+            ? tasks.map((task, index) => (
+                <tr
+                  key={task.id}
+                  className={`border-b last:border-none transition duration-200 hover:bg-indigo-50 ${
+                    index % 2 === 0 ? 'bg-gray-50' : 'bg-white'
+                  }`}
+                >
+                  <td className="px-6 py-3 font-medium text-indigo-600">
+                    {task.id}
+                  </td>
+                  <td className="px-6 py-3">
+                    {editTask === task.id ? (
+                      <input
+                        className="border-2 rounded-md p-1"
+                        placeholder={task.title}
+                        onChange={(e) => setUpdatedTask(e.target.value)}
+                        ref={editInputRef}
+                      />
+                    ) : (
+                      task.title
+                    )}
+                  </td>
+                  <td className="px-6 py-3">
+                    {editTask === task.id ? (
+                      <button
+                        className="p-2 rounded-md mx-1 bg-blue-300 text-gray-700"
+                        onClick={onSaveTask}
+                      >
+                        Save
+                      </button>
+                    ) : (
+                      <button
+                        className="p-2 rounded-md mx-1 bg-blue-300 text-gray-700"
+                        onClick={() => onEditTask(task)}
+                      >
+                        Edit
+                      </button>
+                    )}
+
+                    <button className="p-2 rounded-md mx-1 bg-green-500 text-gray-800">
+                      Complete
+                    </button>
+                    <button className="p-2 rounded-md mx-1 bg-red-400 text-gray-900">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))
+            : 'No Task Added'}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default TaskTable;
